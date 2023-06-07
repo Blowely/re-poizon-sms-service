9f9c4a60cf5c7a381c88e08e7c910eed4eb62b9e
Blowely <maryashin.2014@yandex.ru>
2023-06-08 00:02:43 +0300

    found first working sms sender YEAA!

--- index2.js
+++ index2.js
@@ -3,16 +3,17 @@ const axios = require("axios");
 const https = require("https");
 
 const app = express();
-const port = 3002;
+const port = 3003;
 app.get('/api/sms', function(req, res) {
   const number = req.query.number;
   const text = req.query.code;
   const from = 're:poizon';
 
   const email = 'maryashin.2014@yandex.ru'
-  const apiKey = '5uRZB7O0UstynQgbBeWNnMsn3nbK'
+  const apiKey = 'lE79sqqo0YExECwap62UVSzKd6etgBlkKfxbg8v1sM9NG8NJ0TAGuK7NqMm4'
   const uri = `https://${email}:${apiKey}@gate.smsaero.ru/v2/sms/send`;
-  const test = `https://maryashin.2014@yandex.ru:5uRZB7O0UstynQgbBeWNnMsn3nbK@gate.smsaero.ru/v2/auth`;
+  const test = 'https://auth.terasms.ru/outbox/send/json';
+  //const test = 'https://auth.terasms.ru/outbox/send?login=maryashin.2014@yandex.ru&password=L46Y3G626V&target=79202972447&sender=terasms&message=test`;
 
 
   let url = [
@@ -22,13 +23,23 @@ app.get('/api/sms', function(req, res) {
     '&sign=', from
   ].join('');
 
-  const auth = new Buffer.from(`${email}:${apiKey}`,'base64');
+  const body = {
+    "login": "maryashin.2014@yandex.ru",
+    "password": "L46Y3G626V",
+    "target": 79202972447,
+    "message": "ку-ку",
+    "sender": "terasms.ru"
+
+  }
+
   axios({
-    url: 'https://maryashin.2014@yandex.ru:5uRZB7O0UstynQgbBeWNnMsn3nbK@gate.smsaero.ru/v2/auth',
-    headers: {
-      Authorization: `Basic bWFyeWFzaGluLjIwMTRAeWFuZGV4LnJ1OjV1UlpCN08wVXN0eW5RZ2JCZVdObk1zbjNuYks=`,
+    method: 'POST',
+    url: test,
+    /*headers: {
+      'X-Token': apiKey,
       "Content-Type": 'application/json',
-    }
+    },*/
+    data: body
   })
     .then(res => {
       const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
@@ -36,15 +47,15 @@ app.get('/api/sms', function(req, res) {
       console.log('Date in Response header:', headerDate);
 
       const data = res.data;
-      console.log('res=,', res.statusText);
-      console.log('req=,', res);
+      console.log('res=,', res);
+      console.log('data=,', data);
 
     })
     .catch(err => {
-      console.log('Error: ', err);
+      console.log('Error: ', err.data);
     });
   res.send('hello world');
 });
 
 console.log('app listen on port: ' + port);
-app.listen(3002);
+app.listen(port);
